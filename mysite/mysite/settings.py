baccca9f327d55e579116a86de1d694c58eabccd
Abhishek Gautam <agautam.nk@gmail.com>
2020-11-12 13:51:55 +0530

    increased request size to 50 mb

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DATA_UPLOAD_MAX_MEMORY_SIZE = 52428800 # 50 MB
+FILE_UPLOAD_MAX_MEMORY_SIZE = 52428800 # 50 MB
